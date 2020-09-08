61aa646c78783631e0c1ce50f10a65ac43ea818b
Olivier Girardot <ogmusik@gmail.com>
2020-09-08 08:14:53 +0200

    starting the day with my lexicon and the beginning of a code to store inputs into a json file

--- js/lexicon-controller.js
+++ js/lexicon-controller.js
@@ -7,21 +7,21 @@ function LexiconController ($http) {
         .then(function (response) {
             ctrl.lexicon = response.data.entries;
         });
-    this.entry = [
-        {
-        title: "testing",
-        topics: [],
-        content: "",
-        link: "",
-        mainCategory: "Rails"
-        },{
-        title: "Trying again",
-        topics: [],
-        content: "",
-        link: "",
-        mainCategory: "AngularJS"
-        },
-    ];
+    // this.entry = [
+    //     {
+    //     title: "testing",
+    //     topics: [],
+    //     content: "",
+    //     link: "",
+    //     mainCategory: "Rails"
+    //     },{
+    //     title: "Trying again",
+    //     topics: [],
+    //     content: "",
+    //     link: "",
+    //     mainCategory: "AngularJS"
+    //     },
+    // ];
 };
 
 angular
